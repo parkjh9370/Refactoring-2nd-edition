574e95ac2c227d9c1b63ebd2adab0eae30a4c6ac
Jaehyeon <parkjh9370@naver.com>
2023-05-20 21:54:06 +0900

    08_05 인라인 코드를 함수 호출로 바꾸기

---
+++ chapter_08/05_인라인 코드를 함수 호출로 바꾸기/basic.js
@@ -0,0 +1,7 @@
+let appliesToMass = false;
+
+const states = ['MA', 'RA', 'SA']
+
+for (const s of states) {
+  if (s === 'MA') appliesToMass = true;
+}
---
+++ chapter_08/05_인라인 코드를 함수 호출로 바꾸기/refactoring.ts
@@ -0,0 +1,5 @@
+let appliesToMass = false;
+
+const states = ['MA', 'RA', 'SA']
+
+appliesToMass = states.includes('MA');
