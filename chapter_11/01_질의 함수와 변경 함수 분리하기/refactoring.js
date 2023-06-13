b9bae1ec2c39383b2c07f080dbe579116debfb7b
Jaehyeon <parkjh9370@naver.com>
2023-06-13 22:14:59 +0900

    01_질의 함수와 변경 함수 분리하기

---
+++ chapter_11/01_질의 함수와 변경 함수 분리하기/refactoring.js
@@ -0,0 +1,22 @@
+/**
+ * 질의 함수와 변경 함수 분리해 주기 
+ */
+
+function alertForMiscreant(people) {
+  if (findMiscreant(people) !== '') setOffAlarms();
+}
+
+function findMiscreant(people) {
+  for (const p of people) {
+    if (p === '조커') {
+      return ' 조커';
+    }
+    if (p === '사루만') {
+      return ' 사루만';
+    }
+  }
+  return '';
+}
+
+const found = findMiscreant(people);
+alertForMiscreant(people);
